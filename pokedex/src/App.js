a75a69108b6a9c2bb13a9dea7d86e281d7fb8365
bilginceliktas <114520003+bilginceliktas@users.noreply.github.com>
2023-03-14 19:24:28 +0000

    main page tabs

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabIndex) => {
+    setActiveTab(tabIndex);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <button className={activeTab === 1 ? 'tab-button active' : 'tab-button'} onClick={() => handleTabClick(1)}>Tab 1</button>
+        <button className={activeTab === 2 ? 'tab-button active' : 'tab-button'} onClick={() => handleTabClick(2)}>Tab 2</button>
+        <button className={activeTab === 3 ? 'tab-button active' : 'tab-button'} onClick={() => handleTabClick(3)}>Tab 3</button>
+      </div>
+      <div className="content">
+        <div className={activeTab === 1 ? 'page-container active' : 'page-container'}>Page 1</div>
+        <div className={activeTab === 2 ? 'page-container active' : 'page-container'}>Page 2</div>
+        <div className={activeTab === 3 ? 'page-container active' : 'page-container'}>Page 3</div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
