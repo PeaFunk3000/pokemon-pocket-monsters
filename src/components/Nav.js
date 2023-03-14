7778f458f3f17c5a183d8c3022610a1a056c1678
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-14 21:22:37 +0000

    components created as rfc's

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import "./styles/Nav.css"
 
 export default function Navbar() {
     // create state to determine if navbar expanded or not via boolean
