68a22c9bfc34c315a52fc98a29bc534868d6dd48
bilginceliktas <114520003+bilginceliktas@users.noreply.github.com>
2023-03-15 21:03:24 +0000

    landing page first look

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import "./styles/Landing.css"
 
 export default function Landing() {
   return (
-    <div>Landing</div>
+    <div className="content">
+      <section class="rw-wrapper">
+        <h2 class="rw-sentence">
+          <span>WELLCOME TO</span>
+          <span>Pokemon - Pokedex </span>
+          <div class="rw-words">
+              <span>search for Pokemon</span>
+              <span>info about Trading Cards</span>
+              <span>Compare your Cards</span>
+
+          </div>
+          <span>Gotta Catch 'Em All</span>
+      </h2>
+      </section>
+    </div>
   )
 }
