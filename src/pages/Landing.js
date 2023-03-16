5b58e2a8f92ab48f8a137f62e30f7011fa45c87c
PeaFunk3000 <80793315+PeaFunk3000@users.noreply.github.com>
2023-03-16 20:13:53 +0000

    Bilgin's Landing Page content added

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import ImageShuffle from '../components/ImageShuffle'
 
 export default function Landing() {
   return (
     <div>
-      <ImageShuffle/>
+       <div className="content">
+      <section class="rw-wrapper">
+        <h2 class="rw-sentence">
+          <span>WELLCOME TO</span>
+          <span>Pokemon - Pokedex </span>
+          <div class="rw-words">
+              <span>search for Pokemon</span>
+              <span>info about Trading Cards</span>
+              <span>Compare your Cards</span>
+
+          </div>
+          <span>Gotta Catch 'Em All</span>
+      </h2>
+      </section>
+    </div>
     </div>
   )
 }
