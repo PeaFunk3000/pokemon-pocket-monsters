8f3de4885161fad02209a9591f71de17af176dc3
sevillea <aseville92@gmail.com>
2023-03-19 19:33:20 +0000

    centered-search-bar

--- src/components/SearchBanner.js
+++ src/components/SearchBanner.js
@@ -3,8 +3,9 @@ import "../styles/SearchBanner.css"
 
 function SearchBanner(props) {
     return (
+        <div className = "container flex  justify-center wrap w-screen min-h-fit">
         <div id="banner" >
-            <h1 id="searchLabel" className="text-center">Search</h1>
+            <h1 id="searchLabel" >Search</h1>
             <input
                 id="pokemonSearchInput"
                 type="text"
@@ -12,9 +13,12 @@ function SearchBanner(props) {
                 value={props.searchTerm}
                 onChange={props.handleInputChange}
                 placeholder="Search Pokémon"
+                className="px-4"
+                
             />
-            <button id="submitBtn" onClick={props.handleSubmitForm}>Submit</button>
+            <button id="submitBtn" className="mx-4" onClick={props.handleSubmitForm}>Submit</button>
             <button id="clearBtn" onClick={props.clearScreen}>Clear</button>
+        </div>
         </div>
     );
 }
